5cb653950e72e565473781a5b0e41f8d1b5993aa
Alexander Mironov <goupriver@gmail.com>
2025-01-18 21:52:06 +0700

    0.9

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Необходимые пакеты для работы: wget и sed. 
+# а так же репозиторий в github с файлом каналов user_file
+
 # ставим перенос строки как пробелы и переносы
 IFS=$'\n'
-# WORK_DIR="$HOME"/iptviptv
-WORK_DIR="$HOME"/Documents/script
-DUMP_FILE="$WORK_DIR/dump_iptv"
+WORK_DIR="$HOME"/iptviptv
+DUMP_FILE="$WORK_DIR/IPTV_FILE.M3U"
 USER_FILE="$WORK_DIR/user_file"
 
 
@@ -13,42 +15,42 @@ if [ -f $DUMP_FILE ]; then
 fi
 
 cd $WORK_DIR \
-&& wget -O dump_iptv -c https://gitlab.com/iptv135435/iptvshared/raw/main/IPTV_SHARED.m3u
+&& wget -O IPTV_FILE -c https://gitlab.com/iptv135435/iptvshared/raw/main/IPTV_SHARED.m3u
 
 # удаляем пустые строки из дампа
-# sed -i '/^$/d' $DUMP_FILE
+sed -i '/^$/d' $DUMP_FILE
 
-# line=0
-# group_start=false
+line=0
+group_start=false
 
-# for string_from_dump in $(cat $DUMP_FILE); do
-#     line=$(( $line+1 ))
+for string_from_dump in $(cat $DUMP_FILE); do
+    line=$(( $line+1 ))
 
-#     for channel_from_userlist in $(cat $USER_FILE); do
-#         if [ `echo $string_from_dump | grep -i "$channel_from_userlist"` ]; then
-#             echo $channel_from_userlist
-#             group_start=true
-#             break
-#         fi
-#     done
+    for channel_from_userlist in $(cat $USER_FILE); do
+        if [ `echo $string_from_dump | grep -i "$channel_from_userlist"` ]; then
+            echo $channel_from_userlist
+            group_start=true
+            break
+        fi
+    done
 
-#     if [ `echo $string_from_dump | grep -i "EXTVLCOPT"`  ] && [ $group_start == true ]; then
-#         continue
-#     elif [ `echo $string_from_dump | grep -i "http"` ] && [ $group_start == true ] && [ ! `echo $string_from_dump | grep -o -i "EXTINF"` ]; then 
-#         group_start=false
-#         continue
-#     elif [ `echo $string_from_dump | grep -i "rtmp"` ] && [ $group_start == true ]; then 
-#         group_start=false
-#         continue
-#     fi
+    if [ `echo $string_from_dump | grep -i "EXTVLCOPT"`  ] && [ $group_start == true ]; then
+        continue
+    elif [ `echo $string_from_dump | grep -i "http"` ] && [ $group_start == true ] && [ ! `echo $string_from_dump | grep -o -i "EXTINF"` ]; then 
+        group_start=false
+        continue
+    elif [ `echo $string_from_dump | grep -i "rtmp"` ] && [ $group_start == true ]; then 
+        group_start=false
+        continue
+    fi
 
-#     if [ $group_start == false ]; then
-#         # удалить строку
-#         group_start=false
-#         sed -i "$line"d $DUMP_FILE
-#         line=$(( $line-1 ))
-#     fi
-# done
+    if [ $group_start == false ]; then
+        # удалить строку
+        group_start=false
+        sed -i "$line"d $DUMP_FILE
+        line=$(( $line-1 ))
+    fi
+done
 
 git add .
 git commit -m "$(date +'%e.%m.%Y %R')"
